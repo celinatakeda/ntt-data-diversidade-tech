fa9413445c287aade289e5b093c84a3f74aacaa2
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-11-16 10:07:07 -0300

    commit

---
+++ Angular/trabalhandoComComponentes/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { Button } from './components/button.component';
+import { EntryDataComponent } from './components/entry-data.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, Button, EntryDataComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
