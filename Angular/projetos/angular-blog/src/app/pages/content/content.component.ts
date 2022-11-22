f9ee416d739a8deddce2ea2e2990c726f86904e9
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-11-22 08:51:57 -0300

    commit

--- Angular/projetos/angular-blog/src/app/pages/content/content.component.ts
+++ Angular/projetos/angular-blog/src/app/pages/content/content.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  photoCover:string = "https://www.fortressofsolitude.co.za/wp-content/uploads/2020/01/Tony-Stark-Iron-Man.jpg"
+  contentTitle:string = "NOTÍCIA EXEMPLO"
+  contentDescription:string = "bla blalal"
 
   constructor() { }
 
