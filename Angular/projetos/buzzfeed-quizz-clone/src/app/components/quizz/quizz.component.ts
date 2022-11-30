c08a94afa0373e1c5d9dddff1442b4261d78535f
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-11-30 09:16:35 -0300

    commit

--- Angular/projetos/buzzfeed-quizz-clone/src/app/components/quizz/quizz.component.ts
+++ Angular/projetos/buzzfeed-quizz-clone/src/app/components/quizz/quizz.component.ts
@@ -38,17 +38,31 @@ export class QuizzComponent implements OnInit {
   playerChoose(value:string) {
     this.answers.push(value)
     this.nextStep()
-    console.log(this.answers)
   }
 
-   nextStep() {
+  async nextStep() {
     this.questionIndex+=1
 
     if(this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
   }
 
+  async checkResult(answers:string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ) {
+        return previous
+      } else {
+        return current
+      }
+    })
+    return result
+  }
 }
